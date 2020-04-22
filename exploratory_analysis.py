3dd1ee2f6df3b19eff443d1871a9fe743a0bbebd
Fabio Barros <fbarros640@gmail.com>
2020-04-22 16:55:41 -0300

    Adding some plots

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -52,18 +52,77 @@ rows, cols = genre_filtered_data_frame.shape
 print('Filtered DataFrame shape - rows: {} cols:{}'.format(rows, cols))
 print(genre_filtered_data_frame.head(10))
 
+# Renaming the columns / features to more meaningful names
+genre_filtered_data_frame = genre_filtered_data_frame.rename(
+  columns={'feature1': 'band_1', 'feature8': 'band_2', 'feature15': 'band_3', 'feature22': 'band_4',
+           'feature29': 'band_5', 'feature36': 'band_6', 'feature43': 'band_7', 'feature50': 'band_8',
+           'feature57': 'band_9', 'feature64': 'band_10', 'feature71': 'band_11', 'feature78': 'band_12',
+           'feature85': 'band_13', 'feature92': 'band_14', 'feature99': 'band_15', 'feature106': 'band_16',
+           'feature113': 'band_17', 'feature120': 'band_18', 'feature127': 'band_19', 'feature134': 'band_20',
+           'feature141': 'band_21', 'feature148': 'band_22', 'feature155': 'band_23'}, errors='raise')
 
-plt.rcParams['figure.figsize'] = (25,25)
+
+plt.rcParams['figure.figsize'] = (15,15)
 
 # Let's try to do a correlation analysis with the 23 features:
 correlation = genre_filtered_data_frame.corr()
-sns.heatmap(correlation, annot=True, fmt='g')
-plt.show()
+#sns.heatmap(correlation, annot=True, fmt='g')
+#plt.show()
 
 #Plotting some graphs
-# Plotting a Scatterplot of all shooting zones
-sns.scatterplot(x=genre_filtered_data_frame['feature1'], y=genre_filtered_data_frame['feature148'], marker='o', hue=genre_filtered_data_frame['class'])
-plt.xlabel('feature 1')
-plt.ylabel('feature 155')
+critical_band_1 = 'band_1'
+critical_band_2 = 'band_22'
+
+# Plotting a Scatterplot
+sns.scatterplot(x=genre_filtered_data_frame[critical_band_1], y=genre_filtered_data_frame[critical_band_2], marker='o', hue=genre_filtered_data_frame['class'])
+plt.xlabel(critical_band_1)
+plt.ylabel(critical_band_2)
 plt.title('Music Genres', loc='center')
 plt.show()
+
+# Plotting a scatterplot with transparency
+sns.scatterplot(x=genre_filtered_data_frame[critical_band_1], y=genre_filtered_data_frame[critical_band_2], marker='o', hue=genre_filtered_data_frame['class'], alpha=0.05)
+plt.xlabel(critical_band_1)
+plt.ylabel(critical_band_2)
+plt.title('Music Genres', loc='center')
+plt.show()
+
+# Plotting a KDE Plot
+sns.kdeplot(data=genre_filtered_data_frame[critical_band_1], data2=genre_filtered_data_frame[critical_band_2], shade=True)
+plt.show()
+
+# Plotting a Facet Grid
+# Separating Shots Missed / Made from the right side
+g = sns.FacetGrid(genre_filtered_data_frame, col='class', hue='class')
+# Determine what kind of plot we want
+g = (g.map(plt.scatter, critical_band_1, critical_band_2))
+plt.show()
+
+# Filtering Data by genre
+genre_filtered_data_frame_1 = genre_filtered_data_frame[genre_filtered_data_frame['class'] == 'Rock']
+# plot all the data first
+sns.scatterplot(x=genre_filtered_data_frame[critical_band_1], y=genre_filtered_data_frame[critical_band_2], color='grey')
+# plot the group we want to focus
+sns.scatterplot(x=genre_filtered_data_frame_1[critical_band_1], y=genre_filtered_data_frame_1[critical_band_2], color='blue')
+plt.show()
+
+#Jointplot
+g = sns.jointplot(x=genre_filtered_data_frame[critical_band_1], y=genre_filtered_data_frame[critical_band_2], kind='kde')
+plt.show()
+
+#Sampling
+genre_filtered_data_frame_sample = genre_filtered_data_frame.sample(1000)
+sns.scatterplot(x=genre_filtered_data_frame_sample[critical_band_1], y=genre_filtered_data_frame_sample[critical_band_2], marker='o', hue=genre_filtered_data_frame_sample['class'])
+plt.show()
+
+## Let's deal with continuous and categorical variables together
+# Scatterplot
+rows_data = genre_filtered_data_frame.iloc[:,0:23]
+for row in rows_data.iterrows():
+  sns.scatterplot(x=bark_bands, y=row[1])
+plt.show()
+
+# Strip plot
+#for row in rows_data.iterrows():
+#  sns.stripplot(x=bark_bands, y=row[1], size=2)
+#plt.show()
